adb4b823165e7cb3223af5c8fa860a4210352202
Utkarsh Patel <itismeutkarsh@gmail.com>
2017-01-04 19:32:24 +0530

    Add blank testcase

---
+++ tests/php/test-dummy.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Blank test
+ *
+ * Class Test_Dummy_Class
+ */
+class Test_Dummy_Class extends WP_UnitTestCase {
+
+	public function test_dumb() {
+		$this->assertTrue( true );
+	}
+}
